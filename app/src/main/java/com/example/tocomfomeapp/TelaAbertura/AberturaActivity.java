56397f2d8d2b3828dea3a3699c5fe8d7c2bc3b09
Victor Hugo <vhaporfiro@gmail.com>
2025-03-13 15:09:42 -0300

    Configuração da tela de abertura.

--- app/src/main/java/com/example/tocomfomeapp/TelaAbertura/AberturaActivity.java
+++ app/src/main/java/com/example/tocomfomeapp/TelaAbertura/AberturaActivity.java
@@ -1,6 +1,8 @@
 package com.example.tocomfomeapp.TelaAbertura;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.tocomfomeapp.R;
+import com.example.tocomfomeapp.TelaPrincipal.MainActivity;
 
 public class AberturaActivity extends AppCompatActivity {
 
@@ -17,10 +20,17 @@ public class AberturaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_abertura);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(AberturaActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
     }
 }
